e9457a0c931f39b549243932bb5e4289d2c3fa33
FitriaYesisca <74045336+FitriaYesisca@users.noreply.github.com>
2020-11-07 12:03:16 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -29,5 +29,21 @@ class DatabaseSeeder extends Seeder
             'created_at'=> Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
+        
+         DB::table('authors')->insert([
+            'name' => 'H. G. Wells',
+            'gender' => 'male',
+            'biography' => 'Herbert George Wells adalah seorang novelis, jurnalis, sosiolog, sekaligus sejarawan berkebangsaan Inggris. Dia lahir di Bromley, Kent, Inggris pada tangal 21 September tahun 1866 dan meninggal saat 13 Agustus 1946 di London. Dia terkenal karena novel fiksinya, seperti The Time Machine dan The War of the Worlds, kemudian komik yang dibuatnya, antara lain Tono-Bungay dan The History of Mr. Polly. Bersama dengan Jules Verne, Wells disebut sebagai "Bapak Fiksi Ilmiah".',
+            'created_at'=> Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('authors')->insert([
+            'name' => 'Madeleine L\'Engle',
+            'gender' => 'female',
+            'biography' => 'Seorang penulis Amerika fiksi, non-fiksi, puisi, dan fiksi dewasa muda , termasuk A Wrinkle in Time dan sekuelnya: A Wind in the Door , A Swiftly Tilting Planet , Many Waters , dan An Acceptable Time . Karya-karyanya mencerminkan iman Kristennya dan minatnya yang kuat pada sains modern.',
+            'created_at'=> Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
